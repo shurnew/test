a0be5878b5a195d1a3ee3ac95a72225db60bba09
SOO <SOO@JESIK-Shin>
2018-01-14 22:52:16 +0900

    노력하자 집중과노력이 마지막이다

--- GeomexText/src/kr/geomex/streamTest/Friend.java
+++ GeomexText/src/kr/geomex/streamTest/Friend.java
@@ -5,7 +5,56 @@ public class Friend {
 	String name ="";
 	int age=0;
 	double weight=0;
-	String telePhone;
+	String telephone;
+	
+	public Friend() {
+		this(null,0,0.0,null);
+	}
+
+	public Friend(String name, int age, double weight, String telephone) {
+		super();
+		this.name= name;
+		this.age= age;
+		this.weight= weight;
+		this.telephone= telephone;
+		
+	}
+	
+	public void setName(String name) {
+		this.name= name;
+	}
+	
+
+	public void setTelephone(String telephone) {
+		this.telephone= telephone;
+	}
+	
+	public void setAge(int age) {
+		this.age= age;
+	}
+	
+	public void setWeight(int weight) {
+		this.weight= weight;
+	}
 	
 	
+	public int getAge() {
+		return age;
+	}
+	
+	public double getWeight() {
+		return weight;
+	}
+	
+	@Override
+	public String toString() {
+	
+		return "[name ="+name+", telephone ="+telephone+", age ="+age+", weght ="+weight+"]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		return toString().equals(obj.toString());
+	}
 }
